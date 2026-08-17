0651dfd00a15d96a1a6bf25272eeb2f061f883ac
Deo256-code <deobyamukama0@gmail.com>
2026-08-17 19:15:51 +0300

    Add configuration object for GPS settings

--- config.js
+++ config.js
@@ -1,1 +1,15 @@
+const CONFIG = {
+    GPS_CHANNEL: 3457035,
 
+    // Refresh dashboard every 15 seconds
+    UPDATE_INTERVAL: 15000,
+
+    FIELDS: {
+        latitude: 1,
+        longitude: 2,
+        speed: 3,
+        battery: 4,
+        gsm: 5,
+        status: 6
+    }
+};
